ac6a9269f3587f69aeac443d68564d463a38e664
Vlad <vla.koncha@gmail.com>
2020-09-26 16:09:54 +0700

    check client arg

--- src/main/java/client/Main.java
+++ src/main/java/client/Main.java
@@ -2,7 +2,11 @@ package client;
 
 public class Main {
     public static void main(String[] args) {
-        Client client = new Client(args[0], args[1], Integer.parseInt(args[2]));
-        client.sendFile();
+        try {
+            Client client = new Client(args[0], args[1], Integer.parseInt(args[2]));
+            client.sendFile();
+        } catch (NumberFormatException e) {
+            System.out.println("Enter server's port");
+        }
     }
 }
